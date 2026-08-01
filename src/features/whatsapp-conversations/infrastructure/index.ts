e90b7fd286d1fd84e998b101c458315498ef045c
Taiane Karine <94652702+taianekarine@users.noreply.github.com>
2026-07-31 22:41:08 -0300

    feat: export WhatsApp media gateway

--- src/features/whatsapp-conversations/infrastructure/index.ts
+++ src/features/whatsapp-conversations/infrastructure/index.ts
@@ -1,2 +1,4 @@
 export * from './create-whatsapp-conversation-repository';
+export * from './create-whatsapp-media-content-gateway';
 export * from './tenant-api-whatsapp-conversation-repository';
+export * from './tenant-api-whatsapp-media-content';
